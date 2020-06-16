fc4f2f9e160020d6f7b4004b5be56cf6dbbc303c
nghia-nguyen-dev <60081184+nghia-nguyen-dev@users.noreply.github.com>
2020-06-15 22:27:52 -0400

    more updates

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ const hamburger = document.querySelector('.hamburger')
 const close = document.querySelector('.close')
 const overlay = document.querySelector('.overlay')
 
+// convert nodelist into arr in order to use array iteration methods later
+const chevrons = [...document.querySelectorAll('.chevron')] 
+
+
+// mobile nav functionality
 nav.addEventListener('click',(e) => {
     sideMenu.classList.toggle('slide-out');
     hamburger.classList.toggle('hide-icon');
     close.classList.toggle('hide-icon');
     overlay.classList.toggle('on')
+})
+
+chevrons.forEach(chevron => {
+    chevron.addEventListener('click', function() {
+        const question = this.parentElement.parentElement
+        question.classList.toggle('hidden')
+        chevron.classList.toggle('up');
+    })
 })
